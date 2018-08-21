6f0da4b6e39d7fab2d3f64ffbf5b7d1c61829d00
Csaba Magyar <csabee005@gmail.com>
2018-08-21 11:46:21 +0200

    Testing changes in 2 different stages.

--- test01/src/hu/test/MainTest.java
+++ test01/src/hu/test/MainTest.java
@@ -1,16 +1,23 @@
 package hu.test;
 
+import java.io.StringWriter;
+
 public class MainTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Testing push function.");
-		
+		intWriter(5);
+		stringWriter("valami");
 	}
 	
-	private void stringWriter(String str)
+	private static void stringWriter(String str)
 	{
 		System.out.println(str);
 	}
 
+	private static void intWriter(Integer number) {
+		System.out.println(number.toString());
+	}
+	
 }
