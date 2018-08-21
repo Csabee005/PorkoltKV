f03a443cab84e260dd2b54080ad6c9dc2e72c0e6
Csaba Magyar <csabee005@gmail.com>
2018-08-21 11:34:55 +0200

    First push test from eclipse photon.

---
+++ javaProject01/src/hu/test/TestMain.java
@@ -0,0 +1,10 @@
+package hu.test;
+
+public class TestMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Test01 pushing to master branch.");
+	}
+
+}
